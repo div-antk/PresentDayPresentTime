98d7a66cd11f8aebda752e858a4abe18c9b7af13
div-antk <51283468+owaii@users.noreply.github.com>
2021-02-12 21:59:12 +0900

    とりあえず動く

--- PresentDayPresentTime/ViewController.swift
+++ PresentDayPresentTime/ViewController.swift
@@ -6,12 +6,22 @@
 //
 
 import UIKit
+import SRGNovelGameTexts
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let PdPtText = SRGNovelGameTexts()
+        PdPtText.frame = CGRect(x: 100, y: 100, width: 100, height: 100)
+        PdPtText.textColor = UIColor.orange
+        self.view.addSubview(PdPtText)
+        
+        PdPtText.setText("プレゼント・デイ。プレゼントタイム")
+        
+        PdPtText.startDisplayingText()
     }
 
 
